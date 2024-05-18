27a840cf7552d1994a758bcb571d4767e92e8ce7
coding777e <168589076+coding777e@users.noreply.github.com>
2024-05-18 22:49:22 +0200

    11support.js

--- commands/support.js
+++ commands/support.js
@@ -29,19 +29,16 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/2nkutxc9hK";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/2nkutxc9hK'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n`)
+            .setImage('https://cdn.discordapp.com/attachments/1241154542758920334/1241443347499974778/desktop-wallpaper-anime-city-lights-night-rain-umbrella-sky-artist.jpg?ex=664a37b6&is=6648e636&hm=da6eff6241d35bfc984ee7837b9a4694ab30026d39c49739e5345e542a6e1b70&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
